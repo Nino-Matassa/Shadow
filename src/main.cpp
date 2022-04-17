58975dddcabc89be3d710391347616635b3cd402
Nino-Matassa <nino.matassa@gmail.com>
2022-04-17 08:21:12 +0100

    Elite

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+// https://www.sfml-dev.org/tutorials/2.5/start-linux.php - initial tutorial
+// https://www.youtube.com/watch?v=XqJRm9uxKjo - How to setup video
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
