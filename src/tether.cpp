fe54704cc5aa33c7c11890e67a2c8bcc2a23a34d
Nino-Matassa <nino.matassa@gmail.com>
2022-06-02 07:49:59 +0100

    Modify .gitignore

--- src/tether.cpp
+++ src/tether.cpp
@@ -3,18 +3,3 @@
 
 // using namespace EliteShadow;
 
-// void Tether::addTether(Tether* pTether) {
-//     lstTether.insert(lstTether.end(), pTether);
-// }
-
-// void Tether::removeTether(Tether* pTether) {
-//     lstTether.remove(pTether);
-// }
-
-// void Tether::invalidateTether() {
-//     for(/*const*/ auto& pTether: lstTether) {
-//         pTether->draw();
-//     }
-// }
-
-
